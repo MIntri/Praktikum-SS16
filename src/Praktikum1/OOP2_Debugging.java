5b25a896d78930a444780f8c24c33ae846a38b36
falks <falkschmitz@hotmail.de>
2016-04-01 10:22:51 +0200

    test

--- src/Praktikum1/OOP2_Debugging.java
+++ src/Praktikum1/OOP2_Debugging.java
@@ -8,7 +8,7 @@ package Praktikum1;
  * 
  * @author Jens Lambert
  * @version 1.1
- * debugged by Falk Schmitz tztz
+ * debugged by Falk Schmitz 
  */
 public class OOP2_Debugging {
 	private boolean hatVersatz = false;
